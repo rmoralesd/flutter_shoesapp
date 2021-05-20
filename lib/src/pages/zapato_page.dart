074ecaeba594534a0973343068be0cf8cb159632
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-05-19 19:54:31 -0500

    Adding title and description to the page

--- lib/src/pages/zapato_page.dart
+++ lib/src/pages/zapato_page.dart
@@ -11,7 +11,21 @@ class ZapatoPage extends StatelessWidget {
         const SizedBox(
           height: 20,
         ),
-        ZapatoSizePreview(),
+        Expanded(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                ZapatoSizePreview(),
+                const ZapatoDescripcion(
+                  titulo: 'Nike Air Max 720',
+                  descripcion:
+                      "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
+                ),
+              ],
+            ),
+          ),
+        )
       ],
     ));
   }
--- lib/src/widgets/custom_widgets.dart
+++ lib/src/widgets/custom_widgets.dart
@@ -1,2 +1,3 @@
 export 'package:flutter_shoesapp/src/widgets/custom_appbar.dart';
 export 'package:flutter_shoesapp/src/widgets/zapato_size.dart';
+export 'package:flutter_shoesapp/src/widgets/zapato_desc.dart';
